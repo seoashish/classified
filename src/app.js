2a9082108b1efc65f53c8719dc9a9d596b69a33d
seoashish <seoashishkerketta@gmail.com>
2019-01-24 11:58:19 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ app.set("view engine", "twig");
 
 app.get("/", (req, res) => {
      //res.send("Hello World!");
-       res.render("home", { title: "Jharkhand Classified", message: "Hello there!" });
+       res.render("home", { title: "Classified", message: "Hello there!" });
 });
 
-
+app.get("/user", (req, res) => {
+      res.render("users/login", { title: "User Login Page", message: "Hello User!" });
+});
 
 
 
