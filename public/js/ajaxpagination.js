859c63cab13e20b7fd1781799c249b036b31cca9
Ashish Kerketta <seoashishkerketta@gmail.com>
2019-04-16 18:04:36 +0530

    first commit

--- public/js/ajaxpagination.js
+++ public/js/ajaxpagination.js
@@ -12,14 +12,14 @@ let html;
 
 /* when DOM is ready below function is run */
 $(document).ready(function(){
-  alert("ajax is call");
+ // alert("dom is ready");
 
   /* ajax call */
   getData();
   getAllCategory();
   getAllDistrict();
 
-  getPageNo(page_no);
+//  getPageNo(page_no);
   getCity();
   getCategory();
   getSort();
@@ -33,8 +33,9 @@ function getCity(){
     /* get city value when it's clicked */
     $("ul#citylist").on("click", "div.sub-city > li", function(){
         city = $(this).children("span").data("subcity");
-        alert(city +" is clicked");
+        //alert(city +" is clicked");
         /* ajax call below */
+        getData();
     });
 };
 
@@ -42,8 +43,9 @@ function getCategory(){
     /* get category value when it's clicked */
     $("ul#cat-list").on("click", "div.sub-category > li", function(){
         category = $(this).children("span").data("subcategory");
-        alert(category +" is clicked!");
+        //alert(category +" is clicked!");
         /* ajax call below */
+        getData();
     });
 };
 
@@ -51,8 +53,9 @@ function getSort(){
     /* get sort-by value when it's value is change */
     $("#sort-by").on("change", function(){
         sort_by = $(this).children("option:selected").val();
-        alert("Sort By " + sort_by);
+        //alert("Sort By " + sort_by);
         /* ajax call below */
+        getData();
     });
 };
 
@@ -61,8 +64,9 @@ function getPageLimit(){
     $("#page-limit").on("change", function(){
         page_limit = $(this).children("option:selected").val();
         page_limit = parseInt(page_limit);
-        alert("Page Limit is " + page_limit);
+        //alert("Page Limit is " + page_limit);
         /* ajax call below */
+        getData();
     });
 };
 
@@ -107,19 +111,32 @@ function getAllDistrict(){
 
 /* get data using ajax */
 function getData(){
+//alert("ajax call");
+let query ={};
+if( category != "" && city != "" ){
+    query.subcategory = category;
+    query.city = city;
+}
+if( category == "" && city != "" ){
+    query.city = city;
+}
+if( category != "" && city == "" ){
+    query.subcategory = category;
+}
+console.log(`ajaxsend: ${JSON.stringify(query)} limit: ${page_limit} page: ${page_no} sort: ${sort_by}`);
     $.ajax({
         type: 'POST',
-        url: '/api',
-        data:{
-         limit: page_limit,
-         page: page_no,
-         category: category,
-         city: city,
-         sort: sort_by
+        url: '/api/classified',
+        data: {
+            limit: page_limit,
+            page: page_no,
+            query: query,
+            sort: sort_by
         },
-        dataType: 'json',
+        dataType: "json",
         success: function(jsonData, textStatus){
           console.log(jsonData);
+          classifiedHtml(jsonData.data);
           pagination(jsonData.totalRecord);
         },
         error: function(xhr, textStatus, errorThrown){
@@ -133,7 +150,7 @@ function pagination(totalRecord){
     total_record = totalRecord;
     last_page = Math.ceil(total_record / page_limit);
     half = Math.floor(pagination_button / 2);
-    console.log(total_record+" "+last_page+" "+half);
+    //console.log(total_record+" "+last_page+" "+half);
    /* Remove all html */
    $("div#pagination_list").html("");
     html = `<nav aria-label=""><ul class="pagination">`;
@@ -202,11 +219,9 @@ function categoryHtml(data){
       data.forEach((category) =>{
         let cat = "";
         if(category.category.length > 17){
-            console.log("greater than 12");
             cat += category.category.slice(0, 17);
             cat += "...";
         }else{
-            console.log("less than 12");
             cat += category.category;
         }
         html += `<div class="main-category">
@@ -255,6 +270,38 @@ function districtHtml(data){
         </div>`;
     }
     $("ul#citylist").append(html);
+};
+
+/* classified html manipulate */
+function classifiedHtml(data){
+    let html = "";
+    $("div#classified-list").html("");
+    if(data.length > 0){
+        data.forEach((classified) =>{
+          html += `<div class="d-flex flex-column border my-2">
+          <div class="img_title d-flex p-1">
+              <div class="image" style="width: 200px;">
+                  <img src="/img/${classified.image}" class="img-fluid img-thumbnail" alt="image">
+              </div>
+              <div class="title p-2">
+                  <p class="font-weight-bold text-limegreen mb-2">${classified.title}</p>
+                  <p class="mb-0 mb-md-1"><i class="fas fa-envelope text-limegreen mr-2"></i>${classified.email}</p>
+                  <p class="mb-0 mb-md-1"><i class="fas fa-globe text-limegreen mr-2"></i>${classified.website}</p>
+                  <p class="mb-0"><i class="fas fa-phone text-limegreen mr-2"></i>${classified.mobile || "9876543210"}</p>
+              </div>  
+          </div>
+          <div class="description px-2">
+              <p class="mb-1">${classified.description}</p>
+              <p class="mb-1"><i class="fas fa-map-marker-alt text-limegreen mr-2"></i>${titleCase(classified.city)} <i class="fas fa-th-large text-limegreen ml-5 mr-2"></i>${titleCase(classified.subcategory)}</p>
+          </div>
+      </div>`;
+        });
+    }else{
+         html += `<div class="jumbotron mt-4">
+         <h1 class="display-5">No Classified in this category or city.</h1>
+       </div>`;
+    }
+    $("div#classified-list").append(html);
 };
 
 /* title case function */
--- src/routes/api.js
+++ src/routes/api.js
@@ -7,15 +7,30 @@ const { Category } = require("../models/category_model");
 /******************************************
  ************* Get Classified *************
  *****************************************/
+router.post("/", function(req, res){
+   console.log(req.body);
+   res.send("hello");
+});
 
 router.post("/classified", function(req, res){
    let limit = parseInt(req.body.limit);
    let page = parseInt(req.body.page) < 1 ? 1 : parseInt(req.body.page);
-   let query = {};
-      query.category = req.body.category;
-   //query.city = "";
+   let query = req.body.query;
 
-   Classified.find(query).countDocuments(function(err, count){
+let sort_by = { title: 1 };   
+  if(req.body.sort != "" && req.body.sort == "title"){
+      sort_by = { title: 1 };
+  } 
+  if(req.body.sort != "" && req.body.sort == "website"){
+      sort_by = { website: 1 };
+  }   
+  if(req.body.sort != "" && req.body.sort == "date"){
+      sort_by = { createAt: 1 };
+  }   
+
+
+
+    Classified.find(query).countDocuments(function(err, count){
         if(err){
             res.send(err);
             return;
@@ -24,18 +39,18 @@ router.post("/classified", function(req, res){
         Classified.find(query)
         .limit(limit) // limit
         .skip((page*limit) - limit) //(page*limit) - limit
-        .select("title description email website mobile district category ")
-        .sort({ createAt: -1})
-        .exec((err, category) =>{
+        .select("image title description email website mobile city subcategory -_id")
+        .sort(sort_by)
+        .exec((err, classified) =>{
             if(err){
                 res.send(err);
                 return;
             }
-            res.json({ data: category, totalRecord:count });
+            res.json({ totalRecord:count, data: classified });
         });
-   });
-
-   // format {limit: limit, page: page, totalRecord: 1000}
+   }); 
+ 
+   //res.json({ isEmpty: isEmpty(query), emt1: emt1, sort: sort_by });
 });
 
 
@@ -75,3 +90,13 @@ router.get("/category", (req, res) =>{
 
 
 module.exports = router;
+
+
+/* function check object is empty or not */
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+};
