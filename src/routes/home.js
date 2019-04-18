27e6edc572015939d605686bea429ce2f207e725
Ashish Kerketta <seoashishkerketta@gmail.com>
2019-04-18 11:06:06 +0530

    first commit

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,8 +4,4 @@ router.get("/", (req, res, next) => {
       res.render("showads", { title: "Jharkhand Classified | Classified Ads In Jharkhand | Classified Jharkhand" });
 });
 
-router.get("/view-ad", (req, res, next) => {
-      res.render("adview", { title: "Listing Page" });
-});
-
 module.exports = router;
